70e34c0f3df6d7968938260f9e67ab64c068b3e3
Akash Basavaraju <bmnakash@gmail.com>
2022-10-30 17:11:54 +0530

    feat: csv file format fix

--- src/components/CSVParser.js
+++ src/components/CSVParser.js
@@ -1,22 +1,14 @@
 export const downloadCsv = function (data) {
-  // Creating a Blob for having a csv file format
-  // and passing the data with type
   const blob = new Blob([data], { type: "text/csv" });
 
-  // Creating an object for downloading url
   const url = window.URL.createObjectURL(blob);
 
-  // Creating an anchor(a) tag of HTML
   const a = document.createElement("a");
 
-  // Passing the blob downloading url
   a.setAttribute("href", url);
 
-  // Setting the anchor tag attribute for downloading
-  // and passing the download file name
   a.setAttribute("download", "StudentFormData.csv");
 
-  // Performing a download with click
   a.click();
 };
 
@@ -24,12 +16,24 @@ export const makeCsv = function (objArray) {
   var array = typeof objArray != "object" ? JSON.parse(objArray) : objArray;
   var str = "";
 
+  for (var index in array[0]) {
+    if (typeof array[0][index] !== "object") {
+      if (str !== "") str += ",";
+
+      str += index;
+    }
+  }
+
+  str += "\r\n";
+
   for (var i = 0; i < array.length; i++) {
     var line = "";
     for (var index in array[i]) {
-      if (line !== "") line += ",";
+      if (typeof array[i][index] !== "object") {
+        if (line !== "") line += ",";
 
-      line += array[i][index];
+        line += array[i][index];
+      }
     }
 
     str += line + "\r\n";
